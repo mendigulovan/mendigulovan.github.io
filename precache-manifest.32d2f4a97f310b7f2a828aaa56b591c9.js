c8454cf765b826094e4908fca257cb0b6d54b887
Nagima <nagimamendigulova@gmail.com>
2019-02-27 00:25:36 +0600

    Updates

---
+++ precache-manifest.32d2f4a97f310b7f2a828aaa56b591c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4fbece2f8f93aeb8cb7",
+    "url": "/static/js/main.f4fbece2.chunk.js"
+  },
+  {
+    "revision": "80ab325fabdd11b8f5ce",
+    "url": "/static/js/1.80ab325f.chunk.js"
+  },
+  {
+    "revision": "f4fbece2f8f93aeb8cb7",
+    "url": "/static/css/main.3d0b7e79.chunk.css"
+  },
+  {
+    "revision": "80ab325fabdd11b8f5ce",
+    "url": "/static/css/1.0e2302fb.chunk.css"
+  },
+  {
+    "revision": "c5ec817bfbdfe13eb49af07b5eb72c27",
+    "url": "/index.html"
+  }
+];
